1a0fb06b534ec11532865390cf2b980cc4109527
Felipe André <felipe.tecpro26@gmail.com>
2025-08-19 22:20:31 -0300

    calculos matematicos

--- helloWorld.dart
+++ helloWorld.dart
@@ -1,12 +1,30 @@
 void main(){
-  bool status = false;
-  String resultado;
-  //print(status);
+  /*
+  var nome = "Felipe";
+  var numero = 100;
+  var peso = 95;
+  var status = true;
 
-  //bool isTrue = 1 < 0;
-  //print(isTrue);
+  nome = "String";
 
+  dynamic sobrenome = "Oliveira";
+  dynamic valor = 75.2;
 
-  resultado = status ? "verdadeiro" : "falso";
-  print(resultado);
+  sobrenome = 110;
+  print(sobrenome);
+  */
+  double num1 = 9.5;
+  double num2 = 6;
+  double soma = num1 + num2;
+  print(soma);
+
+  double subtrair = num1 - num2;
+  print(subtrair);
+
+  double multiplicar = num1 * num2;
+  print(multiplicar);
+
+  double dividir = num1 / num2;
+  print(dividir);
+
 }
