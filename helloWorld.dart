65dc12689e6a81f37046acd6edff582f5b8d5d7a
Felipe André <felipe.tecpro26@gmail.com>
2025-08-19 21:32:14 -0300

    hello world dart

---
+++ helloWorld.dart
@@ -0,0 +1,11 @@
+void main(){
+
+  String nome = "Felipe";
+  String sobrenome = "André";
+
+  print(nome + sobrenome);
+
+  String cor = "Azul";
+  String resultado = cor ?? "Sem cor";
+  print(resultado);
+}
