f910c8f57d91fe3aa34e559f4d88c5e2ca431847
Felipe André <felipe.tecpro26@gmail.com>
2025-08-21 21:24:46 -0300

    inserindo cores

--- helloWorld2.dart
+++ helloWorld2.dart
@@ -1,4 +1,13 @@
 void main(){
   List nome = ["Felipe", 55.5, false];
   print(nome);
+
+  List<String> cor = ["amarelo", "azul", "verde"];
+  cor.add("preto");
+  cor.insert(0,"Roxo");
+  //cor.removeLast();
+  //cor.removeAt(0);
+  print(cor.length);
+  print(cor.contains("azul"));
+  print(cor);
 }
