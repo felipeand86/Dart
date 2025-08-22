68cd7f30196a94e3acc9e7c5d959c2286674cc20
Felipe André <felipe.tecpro26@gmail.com>
2025-08-21 21:53:47 -0300

    Decisão com if else

---
+++ helloWorld4.dart
@@ -0,0 +1,22 @@
+//Comparadores
+// > Maior
+// >= Maior igual
+// < Menor
+// <= Menor igual
+// == Igual
+// != Diferente
+
+
+void main(){
+
+  var valor = "diferente";
+
+  if(valor == true){
+    print("O valor é $valor");
+  } else if(valor == false) {
+    print("O valor é $valor");
+   } else {
+    print("O valor não é nenhum dos dois");
+   }
+    
+}
