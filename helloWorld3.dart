e3afe055b622ebc5639515e2d8ac183fb24b906a
Felipe André <felipe.tecpro26@gmail.com>
2025-08-21 21:37:58 -0300

    tipo de variaveis no map

---
+++ helloWorld3.dart
@@ -0,0 +1,10 @@
+void main(){
+
+  Map<int, String> usuario = {1: "Cabra", 2: "Fulano",3: "Ciclano"};
+  print(usuario);
+
+  /*print(usuario["dev"]);
+  //usuario["Idade"] = 25;
+  print(usuario);
+  print(usuario.length);*/
+}
